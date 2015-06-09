37107032203f825eab06100a92ccfdac84ab1cf7
yexithu <502000201@qq.com>
2015-06-09 19:55:58 +0800

    还是发牌

--- src/blackjack/gui/Animation.java
+++ src/blackjack/gui/Animation.java
@@ -5,7 +5,6 @@
  */
 package blackjack.gui;
 
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
@@ -15,21 +14,22 @@ import javax.swing.JPanel;
  * @author Re
  */
 public class Animation {
-    static public class TokenMove implements Runnable{
-        
+
+    static public class TokenMove implements Runnable {
+
         final private JPanel panel;
         private Token token;
         static private double maxScaleRate = 1.3;
         static private int runTime = 400;
-        
+
         public int value;
         public int startX;
         public int startY;
         public int endX;
         public int endY;
         public boolean isAdd;
-        
-        public TokenMove(JPanel pan,Token token, int ex, int ey, boolean isAdd) {
+
+        public TokenMove(JPanel pan, Token token, int ex, int ey, boolean isAdd) {
             this.panel = pan;
             this.value = token.parValue;
             this.startX = token.centerX;
@@ -39,119 +39,112 @@ public class Animation {
             this.endY = ey;
             this.isAdd = isAdd;
             panel.add(token);
-            
+
             new Thread(this).start();
         }
-        
+
         @Override
         public void run() {
 
             double rate = 1;
             token.pauseClickedListener();
 //            BetPanel.defaultTokens.get(token.parValue).pauseClickedListener();
-            for(int i = 0; i < 3; ++i) {
-                rate += (maxScaleRate - 1)/ 3;
+            for (int i = 0; i < 3; ++i) {
+                rate += (maxScaleRate - 1) / 3;
                 int afterX = (int) (token.getX() - Token.radius * (rate - 1));
                 int afterY = (int) (token.getY() - Token.radius * (rate - 1));
-                
+
                 token.setLocation(afterX, afterY);
-                token.setTokenSize((int)(Token.radius * rate));
+                token.setTokenSize((int) (Token.radius * rate));
                 //panel.repaint();
-                 try {
-                    Thread.sleep(runTime / 10);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-            
-            token.setTokenSize(Token.radius);
-            int paveMentX = (endX - token.getX() - Token.radius) / 7;
-            int paveMentY = (endY - token.getY()- Token.radius) / 7;  
-            for(int i = 0; i < 7; ++i) {
-                token.setLocation(token.getX() + paveMentX, token.getY() + paveMentY);
                 try {
                     Thread.sleep(runTime / 10);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            
+
+            token.setTokenSize(Token.radius);
+            int paveMentX = (endX - token.getX() - Token.radius) / 7;
+            int paveMentY = (endY - token.getY() - Token.radius) / 7;
+            for (int i = 0; i < 7; ++i) {
+                token.setLocation(token.getX() + paveMentX, token.getY() + paveMentY);
+                threadSleep( runTime / 10);
+            }
+
             token.centerX = endX;
             token.centerY = endY;
-            
+
             token.continueClickedListener();
 //            BetPanel.defaultTokens.get(token.parValue).continueClickedListener();
-            
-            if(isAdd){
+
+            if (isAdd) {
                 token.setTokenSize(Token.radius - BetPanel.tableTokens.size() / 3);
-            }
-            else {
+            } else {
                 panel.remove(token);
                 BetPanel.defaultTokens.get(token.parValue).setEnabled(true);
                 BetPanel.defaultTokens.get(token.parValue).setVisible(true);
                 panel.repaint();
-            }         
+            }
         }
-        
-         /**
+
+        /**
          *
          * @return
          */
         public Token getToken() {
-            return this.token;               
+            return this.token;
         }
     }
-    
+
     static public class PokerTurn implements Runnable {
 
         static private int runTime = 400;
-            
+
         final private JPanel panel;
         private Poker poker;
         private int centerX;
         private int centerY;
         private double maxScaleRate = 1.15;
+
         public PokerTurn(JPanel panel, Poker poker) {
             this.panel = panel;
             this.poker = poker;
             this.centerX = poker.getX() + poker.getWidth() / 2;
             this.centerY = poker.getY() + poker.getHeight() / 2;
-            
+
             panel.add(this.poker);
             new Thread(this).start();
         }
+
         @Override
         public void run() {
             int perFrame = runTime / 10;
             double angle = 0;
             boolean originSide = poker.isBack;
-            for(int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; ++i) {
                 angle += Math.PI / 10;
                 double rate = Math.cos(angle);
                 rate = Math.abs(rate);
                 double temp = maxScaleRate - 1;
-                double scaleRate = maxScaleRate -Math.abs((temp / 5)* i - temp);
-                if(angle >= Math.PI / 2 && poker.isBack == originSide) {
+                double scaleRate = maxScaleRate - Math.abs((temp / 5) * i - temp);
+                if (angle >= Math.PI / 2 && poker.isBack == originSide) {
                     poker.turnOver();
                 }
-                
-                
-                poker.setCardSize((int) (Poker.width * rate * scaleRate), (int)(Poker.height * scaleRate));
+
+                poker.setCardSize((int) (Poker.width * rate * scaleRate), (int) (Poker.height * scaleRate));
                 poker.setLocation(centerX - poker.getWidth() / 2, centerY - poker.getHeight() / 2);
-                try {
-                    Thread.sleep(perFrame);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
+                threadSleep( perFrame);
+
             }
-            
+
             poker.setCardSize(Poker.width, Poker.height);
             poker.setLocation(centerX - poker.getWidth() / 2, centerY - poker.getHeight() / 2);
         }
     }
-    
+
     static class PokerTurnMove implements Runnable {
+
         static private int turnRunTime = 300;
         static private int moveRunTime = 300;
         final JPanel panel;
@@ -161,7 +154,7 @@ public class Animation {
         private int endX;
         private int endY;
         double maxScaleRate = 1.3;
-        
+
         public PokerTurnMove(JPanel panel, Poker poker, int eX, int eY) {
             this.panel = panel;
             this.poker = poker;
@@ -169,56 +162,48 @@ public class Animation {
             this.centerY = poker.getY() + poker.getHeight() / 2;
             this.endX = eX;
             this.endY = eY;
-            
+
             panel.add(this.poker);
             new Thread(this).start();
         }
-        
+
         @Override
         public void run() {
             int perFrame = turnRunTime / 10;
             double angle = 0;
             boolean originSide = poker.isBack;
-            for(int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; ++i) {
                 angle += Math.PI / 10;
                 double rate = Math.cos(angle);
                 rate = Math.abs(rate);
                 double temp = maxScaleRate - 1;
-                double scaleRate = (maxScaleRate - 1) * i / 10  + 1;
-                if(angle >= Math.PI / 2 && poker.isBack == originSide) {
+                double scaleRate = (maxScaleRate - 1) * i / 10 + 1;
+                if (angle >= Math.PI / 2 && poker.isBack == originSide) {
                     poker.turnOver();
                 }
-                
-                
-                poker.setCardSize((int) (Poker.width * rate * scaleRate), (int)(Poker.height * scaleRate));
+
+                poker.setCardSize((int) (Poker.width * rate * scaleRate), (int) (Poker.height * scaleRate));
                 poker.setLocation(centerX - poker.getWidth() / 2, centerY - poker.getHeight() / 2);
-                try {
-                    Thread.sleep(perFrame);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
+               threadSleep( perFrame);
+
             }
-            
+
             poker.setCardSize(Poker.width, Poker.height);
             poker.setLocation(centerX - poker.getWidth() / 2, centerY - poker.getHeight() / 2);
-            
+
             perFrame = moveRunTime / 10;
             int pavementX = (endX - poker.getX()) / 10;
             int pavementY = (endY - poker.getY()) / 10;
-            for(int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; ++i) {
                 poker.setLocation(poker.getX() + pavementX, poker.getY() + pavementY);
-                 try {
-                    Thread.sleep(perFrame);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                threadSleep( perFrame);
             }
             poker.setLocation(endX, endY);
         }
     }
-    
+
     public static class PokerMove implements Runnable {
+
         static int runTime = 400;
         static double maxScaleRate = 1.3;
         final JPanel panel;
@@ -226,8 +211,8 @@ public class Animation {
         private int endX;
         private int endY;
         private int centerX;
-        private int centerY;   
-        
+        private int centerY;
+
         public PokerMove(JPanel panel, Poker poker, int eX, int eY) {
             this.panel = panel;
             this.poker = poker;
@@ -235,41 +220,42 @@ public class Animation {
             this.endY = eY;
             this.centerX = poker.getX() + poker.getWidth() / 2;
             this.centerY = poker.getY() + poker.getHeight() / 2;
-            
+
             panel.add(this.poker);
             new Thread(this).start();
         }
-        
+
         @Override
         public void run() {
             double rate = 1;
-            for(int i = 0; i < 3; ++i) {
+            for (int i = 0; i < 3; ++i) {
                 rate += (maxScaleRate - 1) / 3;
                 int afterX = (int) (poker.getX() - Poker.width * (rate - 1) / 2);
                 int afterY = (int) (poker.getY() - Poker.height * (rate - 1) / 2);
-                
+
                 poker.setLocation(afterX, afterY);
-                poker.setCardSize((int)(Poker.width * rate), (int)(Poker.height * rate));
+                poker.setCardSize((int) (Poker.width * rate), (int) (Poker.height * rate));
                 //panel.repaint();
-                 try {
-                    Thread.sleep(runTime / 10);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
-                }
+               threadSleep( runTime / 10);
             }
-            
+
             poker.setCardSize(Poker.width, Poker.height);
             int paveMentX = (endX - poker.getX()) / 7;
-            int paveMentY = (endY - poker.getY()) / 7;  
-            for(int i = 0; i < 7; ++i) {
+            int paveMentY = (endY - poker.getY()) / 7;
+            for (int i = 0; i < 7; ++i) {
                 poker.setLocation(poker.getX() + paveMentX, poker.getY() + paveMentY);
-                try {
-                    Thread.sleep(runTime / 10);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                threadSleep( runTime / 10);
             }
             poker.setLocation(endX, endY);
         }
+    }
+
+    static public void threadSleep(int sleepTime) {
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Animation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
--- src/blackjack/gui/PlayPanel.java
+++ src/blackjack/gui/PlayPanel.java
@@ -9,6 +9,8 @@ import static blackjack.gui.BetPanel.cardGui;
 import blackjack.models.Card;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,7 +25,7 @@ public class PlayPanel extends JPanel {
     private ArrayList<Poker> bankerHands;
     private ArrayList<Poker> playerFirstHands;
     private ArrayList<Poker> playerSecondHands;
-    
+
     private PlayerActionListener playerActionListener;
     private boolean isSpilt;
     private int currenSet = 0;
@@ -32,25 +34,46 @@ public class PlayPanel extends JPanel {
     private int totalLeftValue = 0;
     private JLabel betValueLabel;
     private JLabel leftValueLabel;
+    
+    Hashtable<Integer, ArrayList<Poker>> hands;
 
     public PlayPanel() {
         setLayout(null);
         setHandPokers();
         setDefaultCard();
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         ImageIcon background = new ImageIcon("res/playBackground.png");
         g.drawImage(background.getImage(), 0, 0, this);
         
+        Iterator interator = hands.keySet().iterator();
+        
+        while (interator.hasNext()) {
+            Integer next =(Integer) interator.next();
+            ArrayList<Poker> pokers = hands.get(next);
+            for (Poker poker : pokers) {
+                this.remove(poker);
+                this.add(poker);
+                if(pokers.get(0) == poker)
+                    poker.isCoverred = false;
+                else
+                    poker.isCoverred = true;
+            }           
+        }
+        
+        this.remove(cardGui);
+        this.add(cardGui);
+
     }
-    
+
     private void setDefaultCard() {
         cardGui = new Poker(518, 4, new Card(Card.Pattern.DIAMOND, Card.Figure.KNIGHT), true);
         cardGui.isCoverred = false;
         add(cardGui);
-        
+
         cardGui.setClickedListener(new Poker.CardClickedListener() {
             @Override
             public void onCardClicked() {
@@ -58,21 +81,23 @@ public class PlayPanel extends JPanel {
                 //player-ActionListener.onPlayerHit();
                 Card[] cards = new Card[4];
                 for (int i = 0; i < cards.length; i++) {
-                    cards[i] =  new Card(Card.Pattern.DIAMOND, Card.Figure.KNIGHT);
-                    
+                    cards[i] = new Card(Card.Pattern.DIAMOND, Card.Figure.KNIGHT);
+
                 }
-                
+
                 initial(cards);
             }
         });
     }
-    
+
     private void initial(Card[] cards) {
-        Poker playerFirst = new Poker(Poker.defultX, Poker.defaultY, cards[0], true);
-        playerDefaultHands.add(playerFirst);
-        dealAnimation(0, new Poker(cardGui), true);
-        
+        dealCard(0, cards[0], true);
+        dealCard(1, cards[1], true);
+        dealCard(0, cards[2], true);
+        dealCard(1, cards[3], false);
+
     }
+
     private void setLeftValue() {
         this.leftValueLabel.setText(String.valueOf(totalLeftValue));
     }
@@ -85,51 +110,61 @@ public class PlayPanel extends JPanel {
     private void setBetValue() {
         this.betValueLabel.setText(String.valueOf(totalBetValue));
     }
-    
+
     void setPlayerActionListener(PlayerActionListener playerActionListener) {
         this.playerActionListener = playerActionListener;
     }
-    
+
     public interface PlayerActionListener {
+
         void onPlayerStand();
+
         void onPlayerHit();
+
         void onPlayerSpilt();
-        void onPlayerSurrand();       
-        void onPlayerDouble();        
-        void onPlayerTakeInsure();       
+
+        void onPlayerSurrand();
+
+        void onPlayerDouble();
+
+        void onPlayerTakeInsure();
     }
-    
+
     private void dealCard(int index, Card card, boolean toTurn) {
-        
+        Poker tempPoker = new Poker(Poker.defultX, Poker.defaultY, card, true);
+        hands.get(index).add(0, tempPoker);
+        dealAnimation(index, tempPoker, toTurn);
     }
 
     public void dealAnimation(int index, Poker poker, boolean toTurn) {
         int endX = 0, endY = 0;
-        if(index == 0) {
-            endX = (playerDefaultHands.size() - 1) * 25 + 5;endY = 190;            
+        if (index == 0) {
+            endY = 190;
+        } else if (index == 1) {
+            endY = 30;
+        } else if (index == 2) {
+            endY = 190;
+        } else {
+            endY = 190;
         }
-        if(index == 1) {
-            endX = (bankerHands.size() - 1) * 25 + 5;endY = 30;            
-        }
-        if(index == 2) {
-            endX = (playerFirstHands.size() - 1) * 25 + 5;endY = 190;            
-        }
-        else {
-            endX = (playerSecondHands.size() - 1) * 25 + 5;endY = 190;            
-        }
-        
-        if(toTurn) {
-                Animation.PokerTurnMove pokerTurnMove = new Animation.PokerTurnMove(this, poker, endX, endY);
-            } 
-        else {
-                new Animation.PokerMove(this, poker, endX, endY);
+        endX = (hands.get(index).size() - 1) * 25 + 5;
+        if (toTurn) {
+            Animation.PokerTurnMove pokerTurnMove = new Animation.PokerTurnMove(this, poker, endX, endY);
+        } else {
+            new Animation.PokerMove(this, poker, endX, endY);
         }
     }
-    
-    private  void setHandPokers() {
+
+    private void setHandPokers() {
         playerDefaultHands = new ArrayList<>();
         bankerHands = new ArrayList<>();
         playerFirstHands = new ArrayList<>();
         playerSecondHands = new ArrayList<>();
+        
+        hands = new Hashtable<>(4);
+        hands.put(0, playerDefaultHands);
+        hands.put(1, bankerHands);
+        hands.put(2, playerFirstHands);
+        hands.put(3, playerSecondHands);
     }
 }
